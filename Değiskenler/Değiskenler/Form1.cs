7e47347cbde8db79310a64b9d4b289380a6c96be
xERNGLORY <87614317+xERNGLORY@users.noreply.github.com>
2022-06-18 02:03:39 +0300

    :+1:

---
+++ Değiskenler/Değiskenler/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Değiskenler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Ad = ("Eren");
+            MessageBox.Show(Ad);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int yaş = 19;
+            MessageBox.Show(yaş.ToString()); // messageBox sayıları göstermez o yuzden to string koyuyuruz
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Boolean mezuniyet = false;
+            MessageBox.Show(mezuniyet.ToString()); //messageBox boolean göstermez o yuzden to string koyuyuruz
+            MessageBox.Show("Şu an mezun değilim");
+        }
+    }
+}
