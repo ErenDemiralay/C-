0f93e3c42c2d575b5e349e7489d15257402809d5
xERNGLORY <87614317+xERNGLORY@users.noreply.github.com>
2022-06-17 03:35:56 +0300

    :+1:

---
+++ İlk Uygulama/İlk Uygulama/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İlk_Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Tıkladınız";
+            label1.Text = "Anlaşma kabul edildi";
+            MessageBox.Show("Eminmisin","anlaşma kabulu için");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
